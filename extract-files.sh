371202c2da21507203db80fae374ef80f12542ee
Tim Zimmermann <tim@linux4.de>
2024-06-28 20:13:14 +0200

    gta4xl: Remove libhidltransport dependency
    Change-Id: I0113f528379483e72609c015cc44dbde08b151fa

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,9 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/android.hardware.gnss@2.1-impl.so|vendor/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so)
+            "${PATCHELF}" --remove-needed libhidltransport.so "${2}"
+            ;;
         vendor/lib*/libsec-ril.so)
             xxd -p -c0 "${2}" | sed "s/800e40f9e10316aa820c8052e30315aa/800e40f9e10316aa820c8052080080d2/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
